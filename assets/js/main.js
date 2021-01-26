77d517ac8fc4fbfd349704dcfa81c6f48324d69b
arcticmonkey07 <arcticmonkey07@gmail.com>
2021-01-26 22:58:44 +0300

    made a desktop version

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$('.video-slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: $('.video-slider__arrow--left'),
+  nextArrow: $('.video-slider__arrow--right'),
+  dots: false,
+  // appendDots: $('.video-slider__arrows'),
+});
