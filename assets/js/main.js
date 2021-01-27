806b09cf939a9f75de9e93ba2fe5bd5c18698719
arcticmonkey07 <arcticmonkey07@gmail.com>
2021-01-27 23:27:47 +0300

    done responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 $('.video-slider').slick({
   slidesToShow: 2,
   slidesToScroll: 1,
+  swipeToSlide: true,
   arrows: true,
   prevArrow: $('.video-slider__arrow--left'),
   nextArrow: $('.video-slider__arrow--right'),
   dots: false,
-  // appendDots: $('.video-slider__arrows'),
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
 });
